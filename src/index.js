18e9c112318d33a1e50774560ecf15e1a32253ce
Gurpinder Bisla <bisla.gurpinder@gmail.com>
2022-02-25 19:58:08 -0800

    "Added header component, and set up basic routing to all pages"

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import CanadaProvincial from "./pages/CanadaProvincial";
+import GlobalCountry from "./pages/GlobalCountry";
+import CanadaVaccine from "./pages/CanadaVaccine";
+import GlobalCompare from "./pages/GlobalCompare";
+import Welcome from "./pages/Welcome";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ChakraProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="canada/provincial" element={<CanadaProvincial />} />
+        <Route path="canada/vaccinetracker" element={<CanadaVaccine />} />
+        <Route path="global/countrystats" element={<GlobalCountry />} />
+        <Route path="global/comparecountries" element={<GlobalCompare />} />
+      </Routes>
+    </BrowserRouter>
+  </ChakraProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/CanadaProvincial.js
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+const CanadaProvincial = () => {
+  return (
+    <div>
+      <Header />
+      <h1>TODO CANADA PROVINCIAL</h1>
+    </div>
+  );
+};
+
+export default CanadaProvincial;
