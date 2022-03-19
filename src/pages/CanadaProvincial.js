7783abd985903cdac6590d7b781057b602623a7e
Gurpinder Bisla <bisla.gurpinder@gmail.com>
2022-03-18 19:08:00 -0700

    adding calling and filtering of the canada api

--- src/pages/CanadaProvincial.js
+++ src/pages/CanadaProvincial.js
@@ -1,6 +1,34 @@
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
+import axios from "axios";
 
 const CanadaProvincial = () => {
+  const [stats, setStats] = useState({});
+
+  //Get stats on inital loading of the page
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const result = await axios.get("https://api.covid19tracker.ca/summary");
+        const data = result.data.data[0];
+
+        setStats({
+          new_cases: data.change_cases,
+          new_deaths: data.change_fatalities,
+          new_hospitalizations: data.change_hospitalizations,
+          new_recoveries: data.change_recoveries,
+          cases: data.total_cases,
+          deaths: data.total_fatalities,
+          hospitalizations: data.total_hospitalizations,
+          recoveris: data.total_recoveries,
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <div>
       <Header />
