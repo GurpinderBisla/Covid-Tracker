965248c52841f0d97c1c53973dc0f55359e9205b
Daisy Han <zhan08@mylangara.ca>
2022-03-31 00:28:59 -0700

    added background image

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -2,6 +2,7 @@ import { Button } from "@chakra-ui/button";
 import { Flex, Center, VStack, HStack, Heading } from "@chakra-ui/layout";
 import { Link } from "react-router-dom";
 
+
 /**
  * Component that renders the first page the user sees, lets them
  * pick between canadian stats vs global
@@ -9,7 +10,7 @@ import { Link } from "react-router-dom";
  */
 const Welcome = () => {
   return (
-    <VStack>
+    <VStack bgImage="url('https://images.unsplash.com/photo-1647960611306-575d35715bf2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2665&q=80')" bgPosition="centre" bgSize="auto" bgRepeat="no-repeat"> 
       <Heading pt={5} size="3xl" letterSpacing="tighter">
         {" "}
         Another Covid Tracker{" "}
