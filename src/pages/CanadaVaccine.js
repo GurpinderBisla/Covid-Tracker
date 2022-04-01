004e266c98ac2f536ec7c8dd11f2cacf84c2e408
Gurpinder Bisla <bisla.gurpinder@gmail.com>
2022-04-01 12:23:50 -0700

    basic vaccine chart funcanality added

--- src/pages/CanadaVaccine.js
+++ src/pages/CanadaVaccine.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { Select } from "@chakra-ui/select";
 import axios from "axios";
 import GeneralDataCard from "../components/GeneralDataCard";
+import CanadaLineGraph from "../components/CanadaLineGraph";
 
 const provinces = [
   "Alberta",
@@ -21,37 +22,43 @@ const provinces = [
   "Yukon Territory",
 ];
 
-const canadaPopulation2021 = 36991981;
+const populations2021 = {
+  canada: 38526760,
+  NL: 522453,
+  PEI: 166331,
+  "Nova Scotia": 1002586,
+  "New Brunswick": 797102,
+  Quebec: 8639642,
+  Ontario: 14951825,
+  Manitoba: 1390249,
+  Saskatchewan: 1183269,
+  Alberta: 4480486,
+  BC: 5264485,
+  Yukon: 42982,
+  NWT: 45640,
+  Nunavut: 39710,
+};
 
 const blue = "blue.200";
 const red = "red.400";
 const green = "green.200";
 
 const CanadaVaccine = () => {
-  const [province, setProvince] = useState("global");
-  const [vaccineTime, setVaccineTime] = useState(null);
+  const [province, setProvince] = useState(null);
   const [vaccineSummary, setVaccineSummary] = useState(null);
 
   useEffect(() => {
     const getData = async () => {
       try {
-        const data = await axios.get(
-          `https://api.opencovid.ca/timeseries?stat=cumulative_cvaccine&after=${getDateTime(
-            60
-          )}&before=${getDateTime(0)}`
-        );
         const dataSummary = await axios.get("https://api.opencovid.ca/");
-        console.log(dataSummary);
         setVaccineSummary(dataSummary.data.summary[0]);
       } catch (e) {
         console.error(e);
       }
     };
     getData();
-  });
+  }, []);
 
-  console.log(vaccineSummary);
-  if (vaccineSummary === null) return <Box></Box>;
   return (
     <Box>
       <Header />
@@ -66,35 +73,39 @@ const CanadaVaccine = () => {
           <Heading>Canada Total Stats</Heading>
         </Center>
         <Center>
-          <Flex padding={3} justify="center" mg={5}>
-            <Wrap spacing="24px" justify="space-evenly">
-              <WrapItem>
-                <GeneralDataCard
-                  name="Vaccine Distributed"
-                  data={vaccineSummary.cumulative_dvaccine}
-                  bgColor={blue}
-                />
-              </WrapItem>
-              <WrapItem>
-                <GeneralDataCard
-                  name="Vaccine Taken"
-                  bgColor={green}
-                  data={vaccineSummary.cumulative_cvaccine}
-                />
-              </WrapItem>
-              <WrapItem>
-                <GeneralDataCard
-                  name="Population Vaccinated"
-                  data={`% ${Math.floor(
-                    (vaccineSummary.cumulative_cvaccine /
-                      canadaPopulation2021) *
-                      100
-                  )}`}
-                  bgColor={red}
-                />
-              </WrapItem>
-            </Wrap>
-          </Flex>
+          {vaccineSummary !== null ? (
+            <Flex padding={3} justify="center" mg={5}>
+              <Wrap spacing="24px" justify="space-evenly">
+                <WrapItem>
+                  <GeneralDataCard
+                    name="Vaccine Distributed"
+                    data={vaccineSummary.cumulative_dvaccine}
+                    bgColor={blue}
+                  />
+                </WrapItem>
+                <WrapItem>
+                  <GeneralDataCard
+                    name="Vaccine Taken"
+                    bgColor={green}
+                    data={vaccineSummary.cumulative_cvaccine}
+                  />
+                </WrapItem>
+                <WrapItem>
+                  <GeneralDataCard
+                    name="Population Vaccinated"
+                    data={`% ${Math.floor(
+                      (vaccineSummary.cumulative_cvaccine /
+                        populations2021.canada) *
+                        100
+                    )}`}
+                    bgColor={red}
+                  />
+                </WrapItem>
+              </Wrap>
+            </Flex>
+          ) : (
+            <Box></Box>
+          )}
         </Center>
 
         <Center>
@@ -112,16 +123,13 @@ const CanadaVaccine = () => {
             })}
           </Select>
         </Center>
+
+        <Center>
+          {province !== null ? <CanadaLineGraph province="NL" /> : <Box></Box>}
+        </Center>
       </Box>
     </Box>
   );
-};
-
-const getDateTime = (pastDays) => {
-  const date = new Date();
-  date.setDate(date.getDate() - pastDays);
-
-  return `${date.getDay()}-${date.getMonth()}-${date.getFullYear()}`;
 };
 
 export default CanadaVaccine;
